69abdb260982c4f7d85037ff2f6de112f74b1f21
Jahanzaib Ali Bhatti <98645898+jahanzaibali786@users.noreply.github.com>
2023-02-01 15:57:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Feb 1, 2023 03:05:00").getTime();
+var countDownDate = new Date("Feb 2, 2023 00:00:00").getTime();
 
 var countdownfunction = setInterval(function() {
 
