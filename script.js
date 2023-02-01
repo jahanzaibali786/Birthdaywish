c6d306f11f3187107ed3d211aa1e71d54e68b68a
Jahanzaib Ali Bhatti <98645898+jahanzaibali786@users.noreply.github.com>
2023-02-01 16:12:45 +0500

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,7 @@ var countdownfunction = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("jazi").innerHTML = hours + " H : "
-  + minutes + " M : " + seconds + " Sec" + "<br><br><br><br><br> Prepared By Hussy";
+  document.getElementById("jazi").innerHTML = seconds + " Sec" + "<br><br><br><br><br> Prepared By Hussy";
 
  if (distance < 0) 
  {
